a3598f918ba4412a9189867facdecd898742dab2
Peggy <peggy4328@gmail.com>
2023-05-24 21:56:14 +0800

    count money

--- src/components/CartSummary/index.jsx
+++ src/components/CartSummary/index.jsx
@@ -8,14 +8,17 @@ export default function CartSummary() {
     const [isOpen, setIsOpen] = useState(false)
     const cartItems = useSelector(selectCartItems);
     const count = (cartItems.length > 0) ?
-              cartItems.reduce((sum, item) => sum + item.qty, 0)
-              : 0;
+            cartItems.reduce((sum, item) => sum + item.qty, 0)
+            : 0;
+    const countMoney =(cartItems.length > 0) ?
+            cartItems.reduce((sum, item) => sum + item.qty*item.price, 0)
+            : 0; 
     const toggleModal = () => setIsOpen(!isOpen);
 
     return (
         <nav onClick={toggleModal} className={styles.cartSummary}>
             <div className={styles.count}>
-                <p className={styles.count_text}>商品數：{count}件　合計：￥{　}</p>
+                <p className={styles.count_text}>商品數：{count}件　合計：￥{countMoney}</p>
                 <div className={styles.count_icon}>
                     <ShoppingOutlined 
                         style={{ fontSize: '40px', color: '#444444' }}
